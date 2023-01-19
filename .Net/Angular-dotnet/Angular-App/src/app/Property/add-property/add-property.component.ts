411633888d308ee8af322749ef8d627ec4c84e66
Prince Bansal <pbsos38@gmail.com>
2023-01-18 19:48:41 -0700

    Template Driven and reactive Forms

--- .Net/Angular-dotnet/Angular-App/src/app/Property/add-property/add-property.component.ts
+++ .Net/Angular-dotnet/Angular-App/src/app/Property/add-property/add-property.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-
+  @ViewChild('Form') addPropertyForm: NgForm;
   constructor(private route: Router) { }
 
   ngOnInit() {
@@ -17,4 +17,8 @@ export class AddPropertyComponent implements OnInit {
     this.route.navigate(['/']);
   }
 
+  onSubmit(Form?: NgForm){
+  //   console.log(Form); // This will be used when form has onSubmit(Form);
+    console.log(this.addPropertyForm);
+  }
 }
--- .Net/Angular-dotnet/Angular-App/src/app/app.module.ts
+++ .Net/Angular-dotnet/Angular-App/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes,RouterModule, Router } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { propertyCardCompnent } from './Property/Property-card/Property-card.component';
 import { PropertyListComponent } from './Property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { HousingService } from './Service/housing.service';
 import { AddPropertyComponent } from './Property/add-property/add-property.component';
 import { PropertyDetailComponent } from './Property/property-detail/property-detail.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserLoginComponent } from './users/user-login/user-login.component';
+import { UserRegisterComponent } from './users/user-register/user-register.component';
 
 const appRoutes: Routes = [
   {
@@ -18,21 +20,28 @@ const appRoutes: Routes = [
     component: AddPropertyComponent
   },
   {
-    path:'',
-    component:PropertyListComponent
+    path: '',
+    component: PropertyListComponent
   },
   {
-    path:'rent-property',
-    component:PropertyListComponent
+    path: 'rent-property',
+    component: PropertyListComponent
   },
   {
-    path:'property-details/:id',
-    component:PropertyDetailComponent
+    path: 'property-details/:id',
+    component: PropertyDetailComponent
   },
   {
-    path:'**', // this will be default route for all the unknown routes
-    component:PropertyListComponent
-  }
+    path: 'users/login',
+    component: UserLoginComponent
+  }, {
+    path: 'users/register',
+    component: UserRegisterComponent
+  },
+  {
+    path: '**', // this will be default route for all the unknown routes
+    component: PropertyListComponent
+  },
 ]
 
 
@@ -42,13 +51,18 @@ const appRoutes: Routes = [
     propertyCardCompnent,
     PropertyListComponent,
     NavBarComponent,
-    PropertyDetailComponent
-   ],
+    PropertyDetailComponent,
+    AddPropertyComponent,
+    UserLoginComponent,
+    UserRegisterComponent
+  ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
     HousingService
