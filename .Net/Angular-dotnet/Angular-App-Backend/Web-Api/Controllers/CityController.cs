b51d13e04fb6160d04fa32310ca8180c2943865d
Prince Bansal <pbsos38@gmail.com>
2023-02-02 10:39:40 -0700

    Adding Async Call, Adding and deleting data in database

--- .Net/Angular-dotnet/Angular-App-Backend/Web-Api/Controllers/CityController.cs
+++ .Net/Angular-dotnet/Angular-App-Backend/Web-Api/Controllers/CityController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Web_Api.Data;
+using Web_Api.models;
 
 namespace Web_Api.Controllers
 {
@@ -26,11 +28,49 @@ namespace Web_Api.Controllers
             return ("Atlanta");
         }
 
+
         [HttpGet("cities")]
-        public IActionResult GetCities()
+        public async Task<IActionResult> GetCities()
         {
-            var cities = dc.Cities.ToList();
+            var cities = await dc.Cities.ToListAsync();
             return Ok(cities);
+        }
+        //Post api/city/AddCity?name=City4
+        [HttpPost("AddCity")]
+
+        //Post api/city/AddCity/CityName
+        // Varible name should be same as of function parameters
+        [HttpPost("AddCity/{name}")]
+
+        public async Task<IActionResult> AddCity(string name)
+        {
+            City city = new City();
+            city.Name = name;
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        //Post api/city/AddCity/cityObj
+        // For adding a whole JSON object Varible name should be same as of function parameters
+        [HttpPost("AddCityObj")]
+
+        public async Task<IActionResult> AddCity(City cityObj)
+        {
+/*            City city = new City();
+            city.Name = name;
+*/            await dc.Cities.AddAsync(cityObj);
+            await dc.SaveChangesAsync();
+            return Ok(cityObj);
+        }
+
+        [HttpPost("delete/{id}")]
+        public async Task<IActionResult> RemoveCity(int id)
+        {
+            var city = await dc.Cities.FindAsync(id);
+            dc.Cities.Remove(city);
+            await dc.SaveChangesAsync();
+            return Ok(id);
         }
 
     }
